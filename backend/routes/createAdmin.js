a73426ee1c04e149b6f94ff35b2e735f31fd411d
jamtechdev <jamtechba@gmail.com>
2024-08-23 17:09:22 +0530

    reset password api

--- backend/routes/createAdmin.js
+++ backend/routes/createAdmin.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { createAdmin , validateCorporationData } = require('../controllers/createAdmin');
+const {
+  createAdmin,
+  validateCorporationData,
+} = require("../controllers/createAdmin");
+const { resetAdminPassword } = require("../controllers/resetAdminPassword");
 
 // Apply the middleware and controller
-router.post('/create-admin',validateCorporationData ,  createAdmin() );
+router.post("/create-admin", validateCorporationData, createAdmin());
+router.post("/reset-password", resetAdminPassword());
 
 module.exports = router;
