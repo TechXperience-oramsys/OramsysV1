fb20dceab96516eb69d090b380b5e812d690af50
bruno <brunoaugustine2@gmail.com>
2024-08-27 11:39:16 +0100

    push merge

--- frontend/src/config/API/api.config.js
+++ frontend/src/config/API/api.config.js
@@ -8,7 +8,7 @@ let isLocalApi = +port >= 5000;
 // export const API = hostname === 'localhost' ? API_LOCAL : API_PROD
 // export const API = API_LOCAL.host
 
-// export let API;
+export let API;
 // API = 'https://backend.oramsysdev.com/' // this url is for live api
 
 // if(window.location.origin.includes('oramsysdev.com')) {
