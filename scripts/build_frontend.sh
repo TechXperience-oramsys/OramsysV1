551ec4f2095b762be4a3df4a7b8a8cc088c62b80
sandeep kumar <sandeepk6858@gmail.com>
2024-10-01 16:08:12 +0530

    update appspec.yml file

---
+++ scripts/build_frontend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change directory to the frontend
+cd /data/oramsys/frontend
+
+# Create a production build of the frontend
+npm run build
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Change directory to the backend and install dependencies
+cd /data/oramsys/backend
+npm install
+
+# Change directory to the frontend and install dependencies
+cd /data/oramsys/frontend
+npm install
