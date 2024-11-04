b670a68493eaac44c291f2a6a023e639363d3901
bruno <brunoaugustine2@gmail.com>
2024-11-04 15:48:37 +0100

    push cors

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,10 @@ app.use((req, res, next) => {
 });
 
 
-app.use(cors('https://www.oramsysdev.com'));
+// app.use(cors('https://www.oramsysdev.com'));
+app.use(cors({
+    origin: ["https://www.oramsysdev.com"]
+}))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.static('files'))
 
