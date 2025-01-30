64b2cdcd39807b2dab3175d6a7301a570d9f6c91
bruno <brunoaugustine2@gmail.com>
2025-01-30 11:15:45 +0100

    push vercel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,11 +12,6 @@ import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { BsTools } from "react-icons/bs";
 
-// import "slick-carousel/slick/slick-theme.css"
-// import 'react-loading-skeleton/dist/skeleton.css'
-// import 'react-tooltip/dist/react-tooltip.css'
-// import './i18n';
-
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
@@ -56,7 +51,4 @@ root.render(
   </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
