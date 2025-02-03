5146192faf437b0511de35008d8f2b8e6e063d59
techxperienceNG <techxp.ng@gmail.com>
2025-02-03 13:58:15 +0100

    make change to test new dev domain

--- frontend/src/config/API/api.config.js
+++ frontend/src/config/API/api.config.js
@@ -9,8 +9,8 @@
 // export const API = API_LOCAL.host
 
 export let API;
-// API = 'https://backend.oramsysdev.com/' 
-API = 'https://oramsys-server.vercel.app/' 
+API = 'https://backend.oramsysdev.com/' 
+// API = 'https://oramsys-server.vercel.app/' 
 
 // API = 'http://localhost:5003/'
 // if(window.location.origin.includes('oramsysdev.com')) {
